9715c7f0d4067c8139454b5e8f613fd39234477e
Charles Horn <charles.horn@gmail.com>
2018-02-12 11:43:28 +1300

    add solr update_work tests

--- openlibrary/tests/solr/test_update_work.py
+++ openlibrary/tests/solr/test_update_work.py
@@ -35,6 +35,14 @@ class FakeDataProvider:
         return {}
     def get_editions_of_work(self, work):
         return []
+    def get_document(self, key):
+        if key == '/authors/OL24A':
+            return {"key": key, "type": {"key": "/type/redirect"}}
+        if key == '/authors/OL25A':
+            return {"key": key, "type": {"key": "/type/author"}, "name": "Somebody"}
+        return {"key": key, "type": {"key": "/type/delete"}}
+    def find_redirects(self, key):
+        return []
 
 update_work.data_provider = FakeDataProvider()
 
@@ -95,7 +103,7 @@ class Test_build_data:
                     make_edition(publish_date="2000"),
                     make_edition(publish_date="Another 2000"),
 
-                    ## Doesn't seems to be handling this case
+                    ## Doesn't seem to be handling this case
                     make_edition(publish_date="2001-01-02"),
 
                     make_edition(publish_date="01-02-2003"),
@@ -254,3 +262,52 @@ class Test_build_data:
         assert d['author_facet'] ==  ['OL1A Author One', 'OL2A Author Two']
         assert d['author_alternative_name'] == ["Author 1"]
 
+class Test_update_items():
+
+    def test_delete_author(self):
+        requests = update_work.update_author('/authors/OL23A')
+        assert isinstance(requests, list)
+        assert isinstance(requests[0], basestring)
+        assert requests[0] == '<delete><query>key:OL23A</query></delete>'
+
+    def test_redirect_author(self):
+        requests = update_work.update_author('/authors/OL24A')
+        assert isinstance(requests, list)
+        assert isinstance(requests[0], basestring)
+        assert requests[0] == '<delete><query>key:OL24A</query></delete>'
+
+    def test_update_author(self):
+        #TODO: Investigate inconsistent update_author return values:
+        # a list of strings in 2 out of 3 cases, but a list of UpdateRequests in this case:
+        requests = update_work.update_author('/authors/OL25A')
+        assert len(requests) == 1
+        assert isinstance(requests, list)
+        assert isinstance(requests[0], update_work.UpdateRequest)
+        assert requests[0].toxml().startswith('<add>')
+
+    def test_delete_edition(self):
+        editions = update_work.update_edition({'key': '/books/OL23M', 'type': {'key': '/type/delete'}})
+        assert editions == [], "Editions are not indexed by SOLR, expecting empty set regardless of input. Got: %s" % editions
+
+    def test_update_edition(self):
+        editions = update_work.update_edition({'key': '/books/OL23M', 'type': {'key': '/type/edition'}})
+        assert editions == [], "Editions are not indexed by SOLR, expecting empty set regardless of input. Got: %s" % editions
+
+    def test_delete_requests(self):
+        olids = ['/works/OL1W', '/works/OL2W', '/works/OL3W']
+        del_req = update_work.DeleteRequest(olids)
+        assert isinstance(del_req, update_work.DeleteRequest)
+        assert del_req.toxml().startswith("<delete>")
+        for olid in olids:
+            assert "<query>key:%s</query>" % olid in del_req.toxml()
+
+    def test_delete_work(self):
+        del_work = update_work.update_work({'key': '/works/OL23W', 'type': {'key': '/type/delete'}})
+        del_edition = update_work.update_work({'key': '/works/OL23M', 'type': {'key': '/type/delete'}})
+        assert len(del_work) == 1
+        assert len(del_edition) == 1
+        assert isinstance(del_work, list)
+        assert isinstance(del_work[0], update_work.DeleteRequest)
+        assert del_work[0].toxml() == '<delete><query>key:/works/OL23W</query></delete>'
+        assert isinstance(del_edition[0], update_work.DeleteRequest)
+        assert del_edition[0].toxml() == '<delete><query>key:/works/OL23M</query></delete>'
